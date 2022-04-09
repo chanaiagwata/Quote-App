4322b0b1c7634431b7a8c369fc07709dea5ad25d
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-04-09 19:03:16 +0300

    create custom model class

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+  quotes:Quote[] =[
+    new Quote(1, 'First quote', 0, 0),
+    new Quote(2, 'Second quote', 0, 0),
+    new Quote(3, 'Third quote', 0, 0),
+  ]
 
   constructor() { }
 
